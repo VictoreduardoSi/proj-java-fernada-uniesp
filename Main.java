4ee1199b2a63ae6b364c4c1bb7bf9fa9b7c5d82b
Victor Eduardo <127709638+VictoreduardoSi@users.noreply.github.com>
2023-11-28 14:01:40 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,20 +3,20 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        Produto[] produtos = new Produto[4];
+        Pessoa[] pessoas = new Pessoa[4];
 
         String opcao = "";
         int index = 0;
 
-        while (!opcao.equalsIgnoreCase("Sair") && index < produtos.length) {
-            produtos[index] = cadastrarProduto(scanner);
+        while (!opcao.equalsIgnoreCase("Sair") && index < pessoas.length) {
+            pessoas[index] = cadastrarPessoa(scanner);
             index++;
 
-            System.out.println("Produto cadastrado");
-            System.out.println("Pré-visualização do produto:");
-            System.out.println(produtos[index - 1].toString());
+            System.out.println("Pessoa cadastrada");
+            System.out.println("Pré-visualização da pessoa:");
+            System.out.println(pessoas[index - 1].toString());
 
-            System.out.println("Digite 'Sair' para encerrar o programa, 'Editar' para modificar o último produto ou qualquer outra tecla para cadastrar outro produto:");
+            System.out.println("Digite 'Sair' para encerrar o programa, 'Editar' para modificar a última pessoa ou qualquer outra tecla para cadastrar outra pessoa:");
             opcao = scanner.nextLine();
 
             if (opcao.equalsIgnoreCase("Editar")) {
@@ -24,62 +24,52 @@ public class Main {
             }
         }
 
-        for (Produto produto : produtos) {
-            if (produto != null) {
-                System.out.println(produto.toString());
+        for (Pessoa pessoa : pessoas) {
+            if (pessoa != null) {
+                System.out.println(pessoa.toString());
                 System.out.println();
             }
         }
     }
 
-    private static Produto cadastrarProduto(Scanner scanner) {
-        System.out.println("Digite o nome do produto:");
+    private static Pessoa cadastrarPessoa(Scanner scanner) {
+        System.out.println("Digite o nome da pessoa:");
         String nome = scanner.nextLine();
-        System.out.println("Escolha o tipo do produto:");
-        System.out.println("1 - Camisa de time");
-        System.out.println("2 - Camisa de grife");
+        System.out.println("Escolha o tipo de pessoa:");
+        System.out.println("1 - Usuário");
+        System.out.println("2 - Usuário Institucional");
         int tipoEscolhido = scanner.nextInt();
         scanner.nextLine();
 
         if (tipoEscolhido == 1) {
-            return cadastrarCamisaTime(scanner, nome, "Camisa de time");
+            return cadastrarUsuario(scanner, nome, "Usuário");
         } else if (tipoEscolhido == 2) {
-            return cadastrarCamisaGrife(scanner, nome, "Camisa de grife");
+            return cadastrarUsuarioInstitucional(scanner, nome, "Usuário Institucional");
         } else {
-            System.out.println("Opção inválida. Criando produto comum por padrão.");
-            return cadastrarCamisaTime(scanner, nome, "Camisa de time");
+            System.out.println("Opção inválida. Criando usuário por padrão.");
+            return cadastrarUsuario(scanner, nome, "Usuário");
         }
     }
 
-    private static Produto cadastrarCamisaTime(Scanner scanner, String nome, String tipo) {
-        System.out.println("Digite o preço da camisa:");
-        double preco = scanner.nextDouble();
-        System.out.println("Digite o peso da camisa:");
-        double peso = scanner.nextDouble();
-        System.out.println("Digite o código de barras da camisa:");
-        String codigoBarras = scanner.next();
-        System.out.println("Digite o código de identificação da camisa:");
-        String codigoIdentificacao = scanner.next();
-        System.out.println("Digite o time da camisa:");
-        String time = scanner.next();
+    private static Pessoa cadastrarUsuario(Scanner scanner, String nome, String tipo) {
+        System.out.println("Digite o número de inscrição do usuário:");
+        int numeroInscricao = scanner.nextInt();
         scanner.nextLine();
+        System.out.println("Digite a data de nascimento do usuário (formato: dd/mm/yyyy):");
+        String dataNascimento = scanner.nextLine();
 
-        return new CamisaTime(nome, preco, tipo, peso, codigoBarras, codigoIdentificacao, time);
+        return new Usuario(nome, tipo, numeroInscricao, dataNascimento);
     }
 
-    private static Produto cadastrarCamisaGrife(Scanner scanner, String nome, String tipo) {
-        System.out.println("Digite o preço da camisa de grife:");
-        double preco = scanner.nextDouble();
-        System.out.println("Digite o peso da camisa de grife:");
-        double peso = scanner.nextDouble();
-        System.out.println("Digite o código de barras da camisa de grife:");
-        String codigoBarras = scanner.next();
-        System.out.println("Digite o código de identificação da camisa de grife:");
-        String codigoIdentificacao = scanner.next();
-        System.out.println("Digite a marca da camisa de grife:");
-        String marca = scanner.next();
+    private static Pessoa cadastrarUsuarioInstitucional(Scanner scanner, String nome, String tipo) {
+        System.out.println("Digite o número de inscrição do usuário institucional:");
+        int numeroInscricao = scanner.nextInt();
         scanner.nextLine();
+        System.out.println("Digite a data de nascimento do usuário institucional (formato: dd/mm/yyyy):");
+        String dataNascimento = scanner.nextLine();
+        System.out.println("Digite o nome da instituição do usuário institucional:");
+        String instituicao = scanner.nextLine();
 
-        return new CamisaGrife(nome, preco, tipo, peso, codigoBarras, codigoIdentificacao, marca);
+        return new UsuarioInstitucional(nome, tipo, numeroInscricao, dataNascimento, instituicao);
     }
 }
